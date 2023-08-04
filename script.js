b217d64b629f2fcd3ebcc3d91391ec96a558ac6b
Minstuff <manasvialam03@gmail.com>
2023-08-05 02:05:53 +0530

    fixed the image transition

---
+++ script.js
@@ -0,0 +1,11 @@
+// // script.js
+// const originalImage = document.querySelector('.original.hoverable-image');
+// const hoverImage = document.querySelector('.hover-image');
+
+// originalImage.addEventListener('mouseover', () => {
+//     hoverImage.style.opacity = '1';
+// });
+
+// originalImage.addEventListener('mouseout', () => {
+//     hoverImage.style.opacity = '0';
+// });
